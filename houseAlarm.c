d87aa4a7292f098a6dad4f6092af2eb1e41bd9e1
HamzaGKhan <hkhan8815@hotmail.com>
2018-10-27 03:51:31 +0000

    First commit

--- houseAlarm.c
+++ houseAlarm.c
@@ -14,19 +14,19 @@ int main(int argc, char *argv[])
   pinMode (4, INPUT) ;/*Button */
   pullUpDnControl(1,PUD_UP);
   pinMode (5, OUTPUT); /*Buzzer */
-  state = state0();
+  state = 1;
   while (1) 
  {
-  state = state0(); /*Sets to Alarm OFF */
-  state = state1(); /*Sets to Alarm Arming*/
-  state = state2(); /*Sets to Alarm Armed */
+  state = state0(state); /*Sets to Alarm OFF */
+  state = state1(state); /*Sets to Alarm Arming*/
+  state = state2(state); /*Sets to Alarm Armed */
   
-  if(state == state3()){
-    state = state3();
+  if(state == 4){
+    state = 4;
     printf("Alarm_Triggered ");
   }
-  if (state == state4()) {
-   state = state4();
+  if (state == 5) {
+   state = 5;
    printf("Alarm_Sounding");
   }
  }
@@ -35,82 +35,103 @@ int main(int argc, char *argv[])
 
 
 /* sets the state to alarm_Off */
-int state0 ()
+int state0 (int state)
 {
   wiringPiSetup();
-  while (digitalRead(1) == 1)
+  if (state == 1)
   {
+  printf("ALARM_OFF");
+  delay(5000); 
+   while (1)
+   {
+     printf("Alarm 0\n");
      digitalWrite(1,HIGH); /*sets LED 1 ON */
-     delay(1000);         
+     /* delay(1000); */        
      digitalWrite(2,LOW); /*sets LED 2 OFF */
-    delay(1000);
-    digitalWrite(5,LOW); /*mutes the buzzer */
-    delay(1000);
-    break;
-  }
-    printf("Alarm_Off");
+     /*delay(1000);*/
+     digitalWrite(5,LOW); /*mutes the buzzer */
+    /* delay(1000);*/
+    if (digitalRead(1) == 0){
+      printf("Alarm_Arming");
+      return state1(state);
+     }
+   }
+}
     return 0;
 } 
 
 /*sets the state to Alarm_Arming */
-int state1 ()
+int state1 (int state)
 {
+ int i;
  wiringPiSetup();
- while (digitalRead(1) == 1)
+ if (state == 2)
  {
- digitalWrite(5,LOW); /*mutes the buzzer */
-  digitalWrite(1,HIGH); /*sets LED 1 ON */
-  delay(1000);         
-  digitalWrite(1,LOW);/*sets LED 1 OFF */
-  delay(1000) ; 
-  delay(10000);
-  digitalWrite(2,HIGH); /*sets LED 2 ON */
+  printf("Alarm_Arming");
+  digitalWrite(5,LOW); /*mutes the buzzer */
+  for (i = 0; i<5; i++)
+  {        
+  digitalWrite(2,LOW); delay(1000);/*sets LED 2 OFF */
+  digitalWrite(2,HIGH);delay(1000); /*sets LED 2 ON */
+   }
+  return 2;
  }
- printf("ALARM_ARMING ");
- return 1;
+ return 0;
 }
 
 
 /*sets state to Alarm Armed*/
-int state2 ()
+int state2 (int state)
 {
   wiringPiSetup();
+ if (state == 3) {
+  printf("ALARM ARMED\n");
   for (;;) 
-   {
-  digitalWrite(1,LOW);/*sets LED 1 OFF */
-  digitalWrite(2,HIGH); /*sets LED 2 ON */
-  digitalWrite(5,LOW); /*mutes the buzzer */
+    {
+     printf("VALUE OF TRIGGER Alarm: %d\n",digitalRead(0));
+     digitalWrite(1,LOW);/*sets LED 1 OFF */
+     digitalWrite(2,HIGH); /*sets LED 2 ON */
+     digitalWrite(5,LOW); /*mutes the buzzer */
   
-  if (digitalRead(4) == 0) /* If the button is pressed */
-     return state0();
-  if (digitalRead (0) == 0) /*if the alarm is triggered */
-     return state3();
-  printf("Alarm_Armed");
-  }
+     if (digitalRead(4) == 0) /* If the button is pressed */
+     {
+         printf("ALARM IS OFF\n");
+         return 1;
+     }
+     if (digitalRead (0) == 1) /*if the alarm is triggered */
+      {
+        printf("Alarm_TRIGGERED");
+        return 4;
+      }
+   }
+  return 0;
 }
 
 /*sets state to alarm triggered*/
-int state3()
+int state3(int state)
 {
+  int i;
   wiringPiSetup();
   digitalWrite(5,LOW); /*mutes the buzzer */
+  for (i=0;(i<5) && (digitalRead(0) == 1); i++){ 
+  printf("ALARM_TRIGGERED");
   digitalWrite(1,HIGH);/*sets LED 1 ON */
   digitalWrite(2,HIGH); /*sets LED 2 ON */
-  delay(10000);
+  delay(1000);
   digitalWrite(1,LOW);/*sets LED 1 OFF */
   digitalWrite(2,LOW); /*sets LED 2 OFF*/
-  if (digitalRead(4) == 0)
-   return state0();
-  else
-   {
-   printf("ALARM_TRIGGERED");
-   return state4();
+  delay(1000);
   }
+  if (i == 5)
+    return 5;
+  return 1;
 }
 
-int state4()
+/*sets state to alarm sounding */
+int state4(int state)
 {
  wiringPiSetup();
+ printf("ALARM SOUNDING\n");
  for(;;)
  {
   digitalWrite(1,HIGH);/*sets LED 1 ON */
@@ -119,10 +140,12 @@ int state4()
   delay(2000);
   digitalWrite(1,LOW);/*sets LED 1 OFF */
   digitalWrite(2,LOW); /*sets LED 2 OFF*/
+  digitalWrite(5,LOW); /*mutes the buzzer */
   delay(2000);
   printf("INTRUDER ALERT!");
   if (digitalRead(4) == 0)
-   return state0(); 
+   return state0(state); 
+  }
  }
 }
 
